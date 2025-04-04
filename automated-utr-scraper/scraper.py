edfc087bc55177da1dd9820520115ba26e5908e5
Dominick Schulz <130702883+dom-schulz@users.noreply.github.com>
2025-04-03 22:50:47 -0700

    Add files via upload

--- automated-utr-scraper/scraper.py
+++ automated-utr-scraper/scraper.py
@@ -365,3 +365,19 @@ def scrape_utr_history(profile_ids, email, password, offset=0, stop=-1, writer=N
     finally:
         if driver:
             driver.quit()
+
+def setup_driver():
+    """Set up and return a configured Chrome WebDriver."""
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
+    
+    # Use the Chrome binary installed in the container
+    chrome_options.binary_location = '/usr/bin/google-chrome'
+    
+    # Create and return the driver
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
