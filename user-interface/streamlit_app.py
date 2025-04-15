5414f94bf7d7829e2d1dff8d4dd9d0fd626f69df
gspilk <123699226+gspilk@users.noreply.github.com>
2025-04-15 12:53:13 -0700

    Test Geoff Commit_two

--- user-interface/streamlit_app.py
+++ user-interface/streamlit_app.py
@@ -145,7 +145,7 @@ get_agent = Agent(name="Get Agent",
 
 
 # ========== Streamlit UI ==========
-st.title("Tennis Timmy 🤖")
+st.title("Test-Geoff 🤖")
 st.write("Enter two player names and a match location to receive a prediction for the match.")
 
 # Ensure chat history persists across reruns
