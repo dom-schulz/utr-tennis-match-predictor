ec610cfc5270b75f5f43ff2ac15aee0ecd13038b
jztennis <123509325+jztennis@users.noreply.github.com>
2025-04-17 22:51:03 -0700

    sidebar test5?

--- user-interface/streamlit_app.py
+++ user-interface/streamlit_app.py
@@ -154,7 +154,7 @@ st.markdown("""
 
 /* Sidebar custom background and animation */
 section[data-testid="stSidebar"] {
-    background-color: #00B2FF !important;  /* Custom dark blue */
+    background-color: #FFFFFF !important;  /* Custom dark blue */
     transition: all 0.4s ease-in-out;
     z-index: 999;
 }
