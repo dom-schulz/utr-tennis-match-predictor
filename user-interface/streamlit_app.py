3479ac0ceeca8b86255949125462efed7352a7b4
jztennis <123509325+jztennis@users.noreply.github.com>
2025-04-21 12:18:16 -0700

    h2h fix

--- user-interface/streamlit_app.py
+++ user-interface/streamlit_app.py
@@ -296,7 +296,7 @@ with tabs[3]:
         st.metric("Current UTR", round(profile.get("utr", 0), 2))
         st.metric("Win Rate Vs. Lower UTRs", f"{round(profile.get("win_vs_lower", 0) * 100, 2)}%")
         st.metric("Win Rate Vs. Higher UTRs", f"{round(profile.get("win_vs_higher", 0) * 100, 2)}%")
-        st.metric("Head-To-Head Winrate", f"{round(100 * (profile.get("h2h")[player2][0] / profile.get("h2h")[player2][1]), 2)}%")
+        st.metric("Head-To-Head Winrate", f"{profile.get("h2h")[player2][0]} - {profile.get("h2h")[player2][1]}")
 
         if utr_history and dates:
             chart_df = pd.DataFrame({
