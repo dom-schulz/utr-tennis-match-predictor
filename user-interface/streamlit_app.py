6ce83349dea93005c461955ac7bb397caba46543
jztennis <123509325+jztennis@users.noreply.github.com>
2025-04-21 22:52:35 -0700

    fixes

--- user-interface/streamlit_app.py
+++ user-interface/streamlit_app.py
@@ -287,26 +287,6 @@ with tabs[3]:
                 st.metric("Head-To-Head (W-L)", f"{profile.get("h2h")[player2][0]} - {profile.get("h2h")[player2][1]-profile.get("h2h")[player2][0]}")
             except:
                 st.metric("Head-To-Head (W-L)", "0 - 0")
-            utrs = history[player1].get("utr", [])
-            # st.markdown(utrs)
-            dates = history[player1].get("date", [])
-            # st.markdown(dates)
-
-            if utrs and dates:
-                df_plot = pd.DataFrame({
-                    "Date": pd.to_datetime(dates),
-                    "UTR": utrs
-                }).sort_values("Date")
-
-                fig, ax = plt.subplots()
-                ax.plot(df_plot["Date"], df_plot["UTR"], marker='o', linestyle='-')
-                ax.set_title(f"{player1}'s UTR Over Time")
-                ax.set_xlabel("Date")
-                ax.set_ylabel("UTR")
-                ax.grid(True)
-                fig.autofmt_xdate()
-
-                st.pyplot(fig)
 
     def display_graph(player1, player2, history):
         # Plot both UTR histories
