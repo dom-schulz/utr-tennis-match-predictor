ee8a5bc664e0cf93f480a4348ec1ae8b62b46def
jztennis <123509325+jztennis@users.noreply.github.com>
2025-05-01 11:24:53 -0700

    predict fix

--- user-interface/streamlit_app.py
+++ user-interface/streamlit_app.py
@@ -98,7 +98,7 @@ with tabs[0]:
             # match_stub = {  # minimal dict for preprocess()
             #     "p1": p1, "p2": p2, "p1_utr": p1_utr, "p2_utr": p2_utr
             # }
-            vec = preprocess_player_data(p1, p1, profiles)
+            vec = preprocess_player_data(p1, p2, profiles)
             st.markdown(vec)
             
             with torch.no_grad():
